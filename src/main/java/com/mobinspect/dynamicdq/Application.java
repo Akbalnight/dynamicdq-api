52d41619e4922c3cb9b3a9218e622b0a6d19c6e9
Anton Eliseev <av.eliseev@i-teco.ru>
2020-04-08 16:30:37 +0300

    add auto update config

--- src/main/java/com/mobinspect/dynamicdq/Application.java
+++ src/main/java/com/mobinspect/dynamicdq/Application.java
@@ -1,10 +1,26 @@
 package com.mobinspect.dynamicdq;
 
+import com.airtech.dynamicdq.service.ConfigService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@Log4j2
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.mobinspect.dynamicdq", "com.airtech.dynamicdq"})
 public class Application {
+
+	@Autowired
+	ConfigService configService;
+
+	@Scheduled(fixedRate = 5000)
+	private void updateConfig(){
+		configService.getConfigs();
+		log.info("updateConfig");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/mobinspect/dynamicdq/controller/DataController.java
+++ src/main/java/com/mobinspect/dynamicdq/controller/DataController.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,7 +23,7 @@ public class DataController {
 
     @DebugLog
     @PostMapping("/flat/{configName}")
-    public List<ObjectNode> getFlatData(@PathVariable String configName, @RequestBody JsonNode filter, Pageable pageable){
+    public ResponseEntity<List<ObjectNode>> getFlatData(@PathVariable String configName, @RequestBody JsonNode filter, Pageable pageable){
         long startNanos = System.nanoTime();
 
         List<ObjectNode> result = dataService.getFlatData(configName, filter, pageable);
@@ -31,12 +32,15 @@ public class DataController {
         long lengthMillis = TimeUnit.NANOSECONDS.toMillis(stopNanos - startNanos);
         log.info("Time duration query: [{}ms]", lengthMillis);
 
-        return result;
+        if(result == null)
+            return ResponseEntity.badRequest().build();
+        else
+            return ResponseEntity.ok(result);
     }
 
     @DebugLog
     @PostMapping("/hierarchical/{configName}")
-    public List<ObjectNode> getHierarchicalData(@PathVariable String configName, @RequestBody JsonNode filter, Pageable pageable){
+    public ResponseEntity<List<ObjectNode>> getHierarchicalData(@PathVariable String configName, @RequestBody JsonNode filter, Pageable pageable){
         long startNanos = System.nanoTime();
 
         List<ObjectNode> result = dataService.getHierarchicalData(configName, filter, pageable);
@@ -45,6 +49,9 @@ public class DataController {
         long lengthMillis = TimeUnit.NANOSECONDS.toMillis(stopNanos - startNanos);
         log.info("Time duration query: [{}ms]", lengthMillis);
 
-        return result;
+        if(result == null)
+            return ResponseEntity.badRequest().build();
+        else
+            return ResponseEntity.ok(result);
     }
 }
