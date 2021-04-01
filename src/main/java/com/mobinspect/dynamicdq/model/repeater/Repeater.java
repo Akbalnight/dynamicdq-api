a2b5d935ed7c8c345f64944e1660f19ae871779c
Roman Kamnev <r.kamnev@i-teco.ru>
2021-04-01 08:29:02 +0300

    Change Repeater service

--- src/main/java/com/mobinspect/dynamicdq/model/repeater/Repeater.java
+++ src/main/java/com/mobinspect/dynamicdq/model/repeater/Repeater.java
@@ -1,5 +1,6 @@
 package com.mobinspect.dynamicdq.model.repeater;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.mobinspect.dynamicdq.model.ConfigName;
 import com.mobinspect.dynamicdq.model.PeriodName;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Getter
@@ -23,9 +25,9 @@ public class Repeater {
     //Итоговый счёт
     private Integer interval;
     // Дата, до которой выполняется команда
-    private LocalDateTime dateFinish;
+    private OffsetDateTime dateFinish;
     // Следущая дата выполнения комманды
-    private LocalDateTime nextExecution;
+    private OffsetDateTime nextExecution;
     // Текущий счёт
     private Integer currentCount;
     //Итоговый счёт
@@ -36,4 +38,8 @@ public class Repeater {
     private UUID userId;
     //Роль
     private String role;
+    //JSON с данными
+    private JsonNode data;
+    //Значение checkbox
+    private Integer checkboxValue;
 }
--- src/main/java/com/mobinspect/dynamicdq/service/RepeaterService.java
+++ src/main/java/com/mobinspect/dynamicdq/service/RepeaterService.java
@@ -5,22 +5,19 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.irontechspace.dynamicdq.service.DataService;
 import com.irontechspace.dynamicdq.service.SaveDataService;
 import com.mobinspect.dynamicdq.model.detour.Detour;
 import com.mobinspect.dynamicdq.model.detour.DetourNodeDto;
-import com.mobinspect.dynamicdq.model.repeater.DtoToRepeaterConverter;
 import com.mobinspect.dynamicdq.model.repeater.Repeater;
-import com.mobinspect.dynamicdq.model.repeater.RepeaterNodeDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
+import java.time.*;
 import java.util.*;
 
 @Service
@@ -34,19 +31,24 @@ public class RepeaterService {
         this.saveDataService = saveDataService;
     }
 
-    @Scheduled(cron = "${cron.timeset}")
+
+    @Scheduled(cron = "${job.cron.rate}")
     @Transactional
     public void createRepeatableRows() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false);
 
-        String GET_REPEATER = "mobileGetRepeaterData";
+
+        // Получение всех repeaters
+        String GET_REPEATER = "repeaters";
         List<ObjectNode> results = dataService.getFlatData(GET_REPEATER, UUID.fromString("0be7f31d-3320-43db-91a5-3c44c99329ab"), new ArrayList<String>(
                 Collections.singletonList("ROLE_ADMIN")), null, PageRequest.of(0, 10));
 
         for (ObjectNode result : results) {
-            RepeaterNodeDto dto = objectMapper.treeToValue(result, RepeaterNodeDto.class);
-            Repeater e = DtoToRepeaterConverter.convertDtoToRepeater(dto);
+            Repeater e = objectMapper.treeToValue(result, Repeater.class);
             boolean check = checkRepeater(e);
+
             if (check) {
                 switch (e.getConfigName()) {
                     case detours:
@@ -60,27 +62,22 @@ public class RepeaterService {
 
     private void updateRepeaterRow(Repeater e, boolean check, ObjectMapper objectMapper) {
 
-        RepeaterNodeDto dto = DtoToRepeaterConverter.convertRepeaterToDto(e);
-        if (dto.getDateFinish() != null) {
-            dto.setDateFinish(setOffsetDateTimeValue(e.getDateFinish()));
-        }
-
         if (check) {
-            dto.setIsAvailable(true);
+            e.setIsAvailable(true);
             if (e.getNextExecution() != null) {
-                dto.setNextExecution(setOffsetDateTimeValue(createNextDateExecution(e)));
+                e.setNextExecution(createNextDateExecution(e));
             }
 
             if (e.getCurrentCount() != null) {
-                dto.setCurrentCount(e.getCurrentCount() + 1);
+                e.setCurrentCount(e.getCurrentCount() + 1);
             }
         } else {
-            dto.setIsAvailable(false);
+            e.setIsAvailable(false);
         }
 
-        JsonNode repeaterNode = objectMapper.valueToTree(dto);
+        JsonNode repeaterNode = objectMapper.valueToTree(e);
 
-        String SAVE_REPEATER = "mobileRepeaterDataSave";
+        String SAVE_REPEATER = "repeaterDataSave";
         saveDataService.saveData(SAVE_REPEATER, e.getUserId(), new ArrayList<String>(
                 Collections.singletonList(e.getRole())), repeaterNode);
 
@@ -88,15 +85,15 @@ public class RepeaterService {
 
     private boolean checkRepeater(Repeater e) {
 
-        if (e.getIsAvailable()) {
+        if ((e.getIsAvailable()) && (e.getNextExecution() != null)) {
             // При создании не заданы последняя дата и максимальное кол-во повторений
             if (e.getFinalCount() == 0 && e.getDateFinish() == null) {
                 return e.getNextExecution().toLocalDate().isEqual(LocalDate.now());
-            } else if (e.getDateFinish() != null) { // Задана последняя дата
+            } else if ((e.getDateFinish() != null) && (e.getCheckboxValue() == 1)) { // Задана последняя дата
                 if (e.getDateFinish().isAfter(e.getNextExecution())) {
                     return e.getNextExecution().toLocalDate().isEqual(LocalDate.now());
                 }
-            } else if (e.getFinalCount() != 0) {// Задано максимальное кол-во повторений
+            } else if ((e.getFinalCount() != 0) && (e.getCheckboxValue() == 2)) {// Задано максимальное кол-во повторений
                 if (e.getFinalCount() >= e.getCurrentCount()) {
                     return e.getNextExecution().toLocalDate().isEqual(LocalDate.now());
                 }
@@ -106,22 +103,21 @@ public class RepeaterService {
         return false;
     }
 
-    private LocalDateTime createNextDateExecution(Repeater e) {
-        LocalDateTime currentDate = e.getNextExecution();
-        LocalDateTime nextDate = currentDate;
+    private OffsetDateTime createNextDateExecution(Repeater e) {
+        OffsetDateTime nextDate = e.getNextExecution();
 
         switch (e.getPeriodName()) {
             case day:
-                nextDate = currentDate.plusDays(e.getInterval());
+                nextDate = e.getNextExecution().plusDays(e.getInterval());
                 break;
             case week:
-                nextDate = currentDate.plusWeeks(e.getInterval());
+                nextDate = e.getNextExecution().plusWeeks(e.getInterval());
                 break;
             case month:
-                nextDate = currentDate.plusMonths(e.getInterval());
+                nextDate = e.getNextExecution().plusMonths(e.getInterval());
                 break;
             case year:
-                nextDate = currentDate.plusYears(e.getInterval());
+                nextDate = e.getNextExecution().plusYears(e.getInterval());
                 break;
             default:
                 break;
@@ -138,7 +134,7 @@ public class RepeaterService {
 
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        String GET_DETOURS = "mobileGetDetoursData";
+        String GET_DETOURS = "detours";
         List<ObjectNode> results = dataService.getFlatData(GET_DETOURS, e.getUserId(), new ArrayList<String>(
                 Collections.singletonList(e.getRole())), nodeFilter, PageRequest.of(0, 10));
 
@@ -160,9 +156,23 @@ public class RepeaterService {
         dto.setDateFinishPlan(setOffsetDateTimeValue(dateFinishPlan));
         dto.setId(null);
 
+        if (e.getData() != null) {
+            JsonNode actualObj = e.getData();
+
+            dto.setSaveOrderControlPoints(actualObj.get("saveOrderControlPoints").asBoolean());
+            dto.setTakeIntoAccountTimeLocation(actualObj.get("takeIntoAccountTimeLocation").asBoolean());
+            dto.setTakeIntoAccountDateStart(actualObj.get("takeIntoAccountDateStart").asBoolean());
+            dto.setTakeIntoAccountDateFinish(actualObj.get("takeIntoAccountDateFinish").asBoolean());
+
+            dto.setPossibleDeviationLocationTime(actualObj.get("possibleDeviationLocationTime").asInt());
+            dto.setPossibleDeviationDateStart(actualObj.get("possibleDeviationLocationTime").asInt());
+            dto.setPossibleDeviationDateFinish(actualObj.get("possibleDeviationLocationTime").asInt());
+
+        }
+
         JsonNode detourNode = mapper.valueToTree(dto);
 
-        String SAVE_DETOURS = "mobileDetoursSave";
+        String SAVE_DETOURS = "saveDetourForm";
         saveDataService.saveData(SAVE_DETOURS, e.getUserId(), new ArrayList<String>(
                 Collections.singletonList(e.getRole())), detourNode);
     }
